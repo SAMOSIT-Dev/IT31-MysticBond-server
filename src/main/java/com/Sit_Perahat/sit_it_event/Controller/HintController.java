afe8f237dd8f0e9e39d2c6a45af8b06e97a82cd6
Thanaphat-Boonruck <99864890+Thanaphat-Boonrak@users.noreply.github.com>
2025-07-27 21:31:33 +0700

    fix hintController

--- src/main/java/com/Sit_Perahat/sit_it_event/Controller/HintController.java
+++ src/main/java/com/Sit_Perahat/sit_it_event/Controller/HintController.java
@@ -2,11 +2,13 @@ package com.Sit_Perahat.sit_it_event.Controller;
 
 
 import com.Sit_Perahat.sit_it_event.Entity.Hints;
+import com.Sit_Perahat.sit_it_event.JwtProvider.jwtProvider;
 import com.Sit_Perahat.sit_it_event.Service.HintService;
 import com.Sit_Perahat.sit_it_event.dto.DefaultResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,11 +25,12 @@ public class HintController {
 
     private final HintService hintService;
 
-    // รองานว่าจะเอาเป็นยังไง ระหว่าง ให้ใส่รหัสของน้อง หรือ เข้ามา ดู
-    @GetMapping("/{studentId}")
-    public ResponseEntity<DefaultResponse> findHints(@PathVariable String studentId) {
-        Map<String, Object> response = new HashMap<>();
+    private final jwtProvider jwtProvider;
 
+    @GetMapping
+    public ResponseEntity<DefaultResponse> findHints(Authentication authentication) {
+        Map<String, Object> response = new HashMap<>();
+        String studentId = jwtProvider.getUserNameFromAuthentication(authentication);
         try {
             List<String> allHints = hintService.findHints(studentId).stream().map(Hints::getMessage).toList();
             response.put("hints", allHints);
