6b8109137d3fc36eef33a4fe8c7277d67af84c94
Thanaphat-Boonruck <99864890+Thanaphat-Boonrak@users.noreply.github.com>
2025-07-29 01:03:13 +0700

    fix cors Success

--- src/main/java/com/Sit_Perahat/sit_it_event/SecurtyConfig/CorsConfig.java
+++ src/main/java/com/Sit_Perahat/sit_it_event/SecurtyConfig/CorsConfig.java
@@ -15,7 +15,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173", "https://it31-mysticbond.sit.kmutt.ac.th")
+                        .allowedOrigins("http://localhost:5173")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true)
